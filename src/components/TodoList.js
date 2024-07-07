fac16cc2465896e6bb4198999ec6881a7159643d
Razzakh <mrazzakh123@gmail.com>
2024-07-07 07:01:44 +0530

    ff

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import cy from 'cypress'
 const TodoList = (props) => {
    // console.log(props.todos,props.state); 
   return (
@@ -9,9 +8,7 @@ const TodoList = (props) => {
    {props.todos.map((item,i)=>{return(
     <li key={i}>{item} {props.state[i] && <button onClick={()=>{
       props.handlefunc(item,i);
-      cy.get("li > button").eq(0).should("exist");
-      cy.contains("Complete").click();
-      cy.get('li:first-child button').should('not.exist');}}>Complete</button>} </li>
+    }}>Complete</button>} </li>
    )}
    )}
    </ul>

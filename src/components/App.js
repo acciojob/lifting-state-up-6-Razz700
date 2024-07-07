8150892de90e7db18a490e5e9c38dc59af5693de
Razzakh <mrazzakh123@gmail.com>
2024-07-07 07:13:23 +0530

    sd

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,10 @@ import TodoList from "./TodoList";
 const App = () => {
   const todos=['todo1','todo2','todo3'];
   const [todoState,settodoState]= useState([true,true,true]);
-  const [now,setnow]=useState(true);
   const handleComplete=(todoval,k)=>{
 todos.forEach((item,i)=>{
   if (todoval==item) {
     // console.log(todoState);
-  setnow(false);
     settodoState(todoState.map((val,j)=>j==k?false:val));
   //console.log(todoState);}
 }});
@@ -19,7 +17,7 @@ todos.forEach((item,i)=>{
   return (
     <div>
   <h1>Parent Component</h1>
-      <TodoList now={now} todos={todos} handlefunc={handleComplete} state={todoState}/>
+      <TodoList  todos={todos} handlefunc={handleComplete} state={todoState}/>
         {/* Do not remove the main div */}
     </div>
   )
